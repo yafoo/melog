6eda131089cb9138ecb35fa0edaec0963b4e99be
雨思 <yafu>
2019-12-18 13:59:59 +0800

    优化回到顶部按钮样式

--- public/static/melog.js
+++ public/static/melog.js
@@ -7,9 +7,11 @@ $(function(){
         }
     });
 
+    var showGoTop = $('#footer').offset().top - $(window).height() + +$('#footer').css('margin-top').replace(/[^0-9]/g, '');
     $(window).scroll(function(e){
         if($(this).scrollTop() > 200){
             $('#go-top').fadeIn(400);
+            $(this).scrollTop() > showGoTop ? $('#go-top').addClass('go-top') : $('#go-top').removeClass('go-top');
         }else{
             $('#go-top').stop().fadeOut(400);
         }
