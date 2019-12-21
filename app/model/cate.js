9961ab06008d4fb56a28ea9c8b26226f7d5a4811
雨思 <yafu>
2019-12-22 00:27:30 +0800

    优化代码及显示

--- app/model/cate.js
+++ app/model/cate.js
@@ -26,7 +26,7 @@ class Cate extends Model {
     }
 
     async getNav(rows){
-        return await this.db.limit(rows).order('cate_sort', 'asc').cache(600).select();
+        return await this.db.order('cate_sort', 'asc').limit(rows).cache(600).select();
     }
 }
 
