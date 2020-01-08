3b7d8797de76815335a9e6c583379369d376d95a
雨思 <yafu>
2020-01-08 18:26:52 +0800

    升级iijs版本到3.0.2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const {app} = require('iijs');
 
 //server
-app.listen(3000, '0.0.0.0', function(err){
+app.run(3000, '0.0.0.0', function(err){
     if(!err) console.log('http server is ready on 3000');
 });
