aaff7247bb679f3d8fb35604e3eface5e6ed68c2
yafu <zhyaphoo@qq.com>
2020-01-09 18:02:13 +0800

    测试cookie

--- app/controller/admin.js
+++ app/controller/admin.js
@@ -2,6 +2,14 @@ const {Controller} = require('iijs');
 
 class Admin extends Controller {
     async _init() {
+        this.$service.cookie.set('id', '1000');
+        this.$service.cookie.set('name', encodeURIComponent('中文'));
+    }
+
+    async login() {
+        console.log(this.$service.cookie.get('id'));
+        console.log(decodeURIComponent(this.$service.cookie.get('name')));
+        this.display([this.$service.cookie.get('id'), decodeURIComponent(this.$service.cookie.get('name'))]);
     }
 }
 
