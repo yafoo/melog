c7cd4a19d0ada6564e798d43d54e3eaaba0c132b
yafu <zhyaphoo@qq.com>
2020-01-13 17:49:02 +0800

    默认端口改为3001

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const {app} = require('iijs');
 
 //server
-app.run(3000, '0.0.0.0', function(err){
-    if(!err) console.log('http server is ready on 3000');
+app.run(3001, '0.0.0.0', function(err){
+    if(!err) console.log('http server is ready on 3001');
 });
