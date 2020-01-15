23dfc3bb7a51a46f1458e1e76158f15eec79ad74
www <www@i-i.me>
2020-01-15 18:21:55 +0800

    后台文章列表去掉缓存

--- admin/model/article.js
+++ admin/model/article.js
@@ -27,7 +27,7 @@ class Article extends Model {
         const pageSize = this.$$pagination.options.pageSize;
         const [total, list] = await Promise.all([
             this.db.where(condition).cache(60).count('id'),
-            this.db.table('article a').field('a.id,a.cate_id,a.user_id,a.title,a.writer,a.click,a.description,a.add_time,c.cate_name,c.folder').join('cate c', 'a.cate_id=c.id').where(condition).order('a.id', 'desc').page(page, pageSize).cache(60).select()
+            this.db.table('article a').field('a.id,a.cate_id,a.user_id,a.title,a.writer,a.click,a.description,a.add_time,c.cate_name,c.folder').join('cate c', 'a.cate_id=c.id').where(condition).order('a.id', 'desc').page(page, pageSize).select()
         ]);
         return [total, list];
     }
