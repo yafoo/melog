7548ec8166b3802df7113e007eb3b264174b967c
yusi <zhyaphoo@qq.com>
2020-02-04 18:42:55 +0800

    添加分类管理

---
+++ admin/controller/cate.js
@@ -0,0 +1,64 @@
+const {utils} = require('iijs');
+const Base = require('./base');
+
+class Cate extends Base {
+    async index() {
+        const list = await this.$model.cate.getList(undefined, 100, 'cate_sort', 'asc');
+        this.assign('list', list);
+        await this.fetch();
+    }
+
+    async add() {
+        const id = parseInt(this.ctx.query.id);
+        let cate = {};
+        if(id) {
+            cate = await this.$model.cate.getOne({id});
+        }
+
+        this.assign('cate', cate);
+        await this.fetch();
+    }
+
+    async save() {
+        if(this.ctx.method != 'POST'){
+            return this.error('非法请求！');
+        }
+
+        const data = this.ctx.request.body;
+        const id = data.id;
+        delete data.id;
+        if(id) {
+            const result = await this.$model.cate.update(data, {id});
+            if(result) {
+                this.success('保存成功！', 'index');
+            } else {
+                this.error('保存失败！');
+            }
+        } else {
+            const result = await this.$model.cate.add(data);
+            if(result) {
+                this.success('新增成功！', 'index');
+            } else {
+                this.error('保存失败！');
+            }
+        }
+    }
+
+    async delete() {
+        const id = parseInt(this.ctx.query.id);
+        const cate = await this.$model.cate.getOne({id});
+        if(!cate) {
+            return this.error('分类不存在！');
+        }
+
+        const result = await this.$model.cate.delete({id});
+
+        if(result) {
+            this.success('删除成功！', 'index');
+        } else {
+            this.error('删除失败！');
+        }
+    }
+}
+
+module.exports = Cate;
--- admin/model/cate.js
+++ admin/model/cate.js
@@ -9,10 +9,6 @@ class Cate extends Model {
         return await this.db.where(condition).find();
     }
 
-    async getOneByID(id){
-        return await this.getOne({id});
-    }
-
     async add(data){
         return await this.db.insert(data);
     }
