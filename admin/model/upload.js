26fe86362c348a8f7c75213d25a382abe74eb4a9
www <www@i-i.me>
2022-01-04 17:26:26 +0800

    后台文件列表显示原始文件大小

--- admin/model/upload.js
+++ admin/model/upload.js
@@ -11,12 +11,20 @@ class Upload extends Model
             list.map(item => {
                 item.image = site_config.upload + item.image;
                 item.thumb = site_config.upload + item.thumb;
-                item.size_text = item.size < 1024 ? item.size + 'B' : item.size < 1024 * 1024 ? (item.size/1024).toFixed(1) + 'KB' : (item.size/1024/1024).toFixed(1) + 'MB';
+                if(item.original) {
+                    item.original = site_config.upload + item.original;
+                }
+                item.size_text = this.getFileSize(item.size);
+                item.origin_size_text = this.getFileSize(item.origin_size);
                 return item;
             });
         }
 
         return [list, pagination];
+    }
+
+    getFileSize(size) {
+        return !size ? '' : size < 1024 ? size + 'B' : size < 1024 * 1024 ? (size/1024).toFixed(1) + 'KB' : (size/1024/1024).toFixed(1) + 'MB';
     }
 }
 
