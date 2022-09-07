ddf2c1b7566e83d160ddbf2b89c5a90ac6c02be3
雨思 <zhyaphoo@qq.com>
2022-09-07 18:27:34 +0800

    紧急修复链接bug

--- config/routes.js
+++ config/routes.js
@@ -3,9 +3,9 @@ routes = [
     {url: '/article/:id.html', path: 'article/article', name: 'article'},
     {url: '/search', path: 'search/search', name: 'search'},
     {url: '/special/:id.html', path: 'special/special', name: 'special'},
-    {url: '/:cate((?!admin|install)[^\/]+)/', path: 'cate/cate', name: 'cate'}, // 会匹配到自定义后台地址，所以程序内需执行this.$next()
-    {url: '/:cate((?!admin|install)[^\/]+)/list_:page.html', path: 'cate/cate', name: 'cate_page'},
-    {url: '/:app((?!install)[^\/]+)/:controller?/:action?', path: 'admin/auth/index', type: 'middleware'} // 后台验证中间件
+    {url: '/:cate((?!admin|install)[^/]+)/', path: 'cate/cate', name: 'cate'}, // 会匹配到自定义后台地址，所以程序内需执行this.$next()
+    {url: '/:cate((?!admin|install)[^/]+)/list_:page.html', path: 'cate/cate', name: 'cate_page'},
+    {url: '/:app((?!install)[^/]+)/:controller?/:action?', path: 'admin/auth/index', type: 'middleware'} // 后台验证中间件
 ];
 
 module.exports = routes;
