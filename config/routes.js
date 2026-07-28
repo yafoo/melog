d78fe90d26d160d2fdf3158ad19eee53d0d413f8
yafu <zhyaphoo@qq.com>
2026-07-28 15:04:00 +0800

    优化路由配置

--- config/routes.js
+++ config/routes.js
@@ -3,10 +3,10 @@ const routes = [
     {url: '/article/:id.html', path: 'app/article/article', name: 'article'},
     {url: '/search', path: 'app/search/search', name: 'search'},
     {url: '/special/:id.html', path: 'app/special/special', name: 'special'},
-    {url: '/:cate/', path: 'app/cate/cate', name: 'cate'}, // 会匹配到admin|install等地址，所以程序内需执行this.$next()
-    {url: '/:cate/list_:page.html', path: 'app/cate/cate', name: 'cate_page'},
     {url: '/install', path: 'install/index/index'},
     {url: '/admin', path: 'admin/index/index'},
+    {url: '/:cate/', path: 'app/cate/cate', name: 'cate'}, // 会匹配到admin|install等地址，所以程序内需执行this.$next()
+    {url: '/:cate/list_:page.html', path: 'app/cate/cate', name: 'cate_page'},
 ];
 
 module.exports = routes;
