f5d3ffbc4233809d6787f708ad0830a23b13f56a
yafu <zhyaphoo@qq.com>
2026-08-03 18:08:42 +0800

    忽略客户端提前断开连接的错误

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const {App, Logger} = require('jj.js');
 
+// 忽略客户端提前断开连接的错误
+process.on('uncaughtException', (err) => {
+    // @ts-ignore
+    if (err.code === 'ERR_STREAM_PREMATURE_CLOSE') return;
+    throw err;
+});
+
 //server
 const port = 3003;
 const app = new App();
